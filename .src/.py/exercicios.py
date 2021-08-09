cc7671ebdd87324aad0ad11320f8652c68882d13
Jhonata Peres <jhonatapers@gmail.com>
2021-08-09 20:04:35 -0300

    Ex 1, 2 e 3

---
+++ .src/.py/exercicios.py
@@ -0,0 +1,25 @@
+#1
+def fatorial(n):
+    if(n < 0):
+        raise Exception('Menor que zero')
+    if(n == 0 or n == 1):
+        return 1
+    return n * fatorial(n - 1)
+
+#2
+def somatorio(n):
+    if(n < 0):
+        raise Exception('Menor que zero')
+    if(n == 0):
+        return 0
+    return n + somatorio(n- 1)
+
+#3
+def fibonacci(n):
+    if(n < 1):
+        raise Exception('Menor que um')
+    if(n == 1):
+        return 0
+    if(n == 2):
+        return 1
+    return fibonacci(n -1) + fibonacci(n -2)
